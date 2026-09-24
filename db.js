8861086c132eeb39316357f9fd0565eebba98574
Prathyusha <prathyushapathu29@gmail.com>
2026-09-24 20:54:30 +0530

    Connect application to Aiven MySQL

--- db.js
+++ db.js
@@ -2,15 +2,19 @@ const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
 
-    host: process.env.DB_HOST || "127.0.0.1",
+    host: process.env.DB_HOST,
 
-    user: process.env.DB_USER || "root",
+    user: process.env.DB_USER,
 
-    password: process.env.DB_PASSWORD || "DBMSproj@123",
+    password: process.env.DB_PASSWORD,
 
-    database: process.env.DB_NAME || "employee_attendance",
+    database: process.env.DB_NAME,
 
-    port: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT,
+
+    ssl: {
+        rejectUnauthorized: false
+    },
 
     waitForConnections: true,
 
